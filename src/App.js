0916f60cd695ad030b76e16bb26a24dd5ea982bb
iCode <gbengaobafemi1@gmail.com>
2021-09-09 14:01:59 +0100

    deployment

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import React from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/pages/Home';
-import './App.css';
-import Abouts from './components/pages/About';
-import Careers from './components/pages/Careers';
-import News from './components/pages/News';
-import Contact from './components/pages/Contact';
-import GMTP from './components/pages/GMTP';
-import Search from './components/pages/Search';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import "./App.css";
+import Abouts from "./components/pages/About";
+import Careers from "./components/pages/Careers";
+import News from "./components/pages/News";
+import Contact from "./components/pages/Contact";
+import GMTP from "./components/pages/GMTP";
 
 function App() {
   return (
-<>
-<Router>
-    <Navbar />
-  <Switch>
-      <Route path='/' exact component={Home} />   
-      <Route path='/about' exact component={Abouts} />   
-      <Route path='/careers' exact component={Careers} />   
-      <Route path='/news' exact component={News} />   
-      <Route path='/contact' exact component={Contact} />   
-      <Route path='/careers/traniees' exact component={GMTP} />   
-      <Route path='/search' exact component={Search} />   
-y
-     </Switch>
-   </Router>
-</>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={Abouts} />
+          <Route path="/careers" exact component={Careers} />
+          <Route path="/news" exact component={News} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/careers/traniees" exact component={GMTP} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
